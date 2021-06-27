2c8a84cab57479ca23e85544c58bd9a09e04b8e7
Mainul Dip <mainuldip@gmail.com>
2021-06-28 03:33:19 +0600

    Block Variation Extending

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 import { registerBlockVariation } from '@wordpress/blocks'
 import { __ } from '@wordpress/i18n'
 
-registerBlockVariation('core/embed', {
+const innerBlocks = [
+  [
+    'core/column',
+    {},
+    [
+      [
+        'core/heading',
+        {
+          backgroundColor: 'yellow',
+          placeholder: 'Project Title',
+          attributes: { style: 'background-color: yellow;' }
+        }
+      ]
+    ]
+  ],
+  [
+    'core/column',
+    {},
+    [
+      ['core/heading', { content: 'Client' }],
+      ['core/paragraph', { placeholder: 'Enter client info' }]
+    ]
+  ],
+  [
+    'core/column',
+    {},
+    [
+      ['core/heading', { content: 'My Role' }],
+      ['core/paragraph', { placeholder: 'Describe your role' }]
+    ]
+  ]
+]
+
+registerBlockVariation('core/group', {
   name: __('Custom Block Variation'),
-  title: __('Custom Block Variation')
+  title: __('Custom Block Variation'),
+  icon: 'portfolio',
+  scope: ['inserter'],
+  innerBlocks
 })
